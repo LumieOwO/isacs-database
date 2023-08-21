28ff61261ceab41af3e9849bd0b60a1208ec749b
LumieOwO <hxhd10525@gmail.com>
2023-08-21 20:25:38 +0300

    No fucking idea LMFAOOO

--- databse.py
+++ databse.py
@@ -7,12 +7,9 @@ app = Flask(__name__)
 port = 3001
 
 
-
 class DataHandler:
     def __init__(self, data_file_path):
         self.data_file_path = data_file_path
-
-
 
     @lru_cache(maxsize=None)
     def read_data(self):
@@ -41,57 +38,57 @@ class DataHandler:
             print("Error writing data:", e)
 
     def get_data(self):
-            directory = unquote(request.headers.get("Directory", ""))
-            data = self.read_data()
-            response_data = data.get(
-                directory,
-                {
-                    "Success": False,
-                    "StatusCode": 404,
-                    "StatusMessage": "Data Not Found!",
-                },
-            )
-
-            if response_data == {
+        directory = unquote(request.headers.get("Directory", ""))
+        data = self.read_data()
+        response_data = data.get(
+            directory,
+            {
                 "Success": False,
                 "StatusCode": 404,
                 "StatusMessage": "Data Not Found!",
-            }:
-                response_body = jsonify(response_data)
-                return response_body, 404
+            },
+        )
 
-            success_response_data = {
-                "Success": True,
-                "StatusCode": 200,
-                "StatusMessage": "Data Successfully Found!",
-                "Body": response_data,
-            }
-            success_response_body = jsonify(success_response_data)
+        if response_data == {
+            "Success": False,
+            "StatusCode": 404,
+            "StatusMessage": "Data Not Found!",
+        }:
+            response_body = jsonify(response_data)
+            return response_body, 404
 
-            return success_response_body, 200
+        success_response_data = {
+            "Success": True,
+            "StatusCode": 200,
+            "StatusMessage": "Data Successfully Found!",
+            "Body": response_data,
+        }
+        success_response_body = jsonify(success_response_data)
+
+        return success_response_body, 200
 
     def modify_data(self, modify_func):
-            try:
-                data = self.read_data()
-                request_data = request.get_json()
-                print(request_data)
-                directory = unquote(request.headers.get("Directory", ""))
-                if directory is None:
-                    return (
-                        jsonify({"success": False, "error": "Invalid request data"}),
-                        400,
-                    )
-
-                modified_data = modify_func(data, directory, request_data)
-                self.write_data(modified_data)
-                self.read_data.cache_clear()
-                return jsonify({"success": True})
-            except Exception as e:
-                print("Error modifying data:", e)
+        try:
+            data = self.read_data()
+            request_data = request.get_json()
+            print(request_data)
+            directory = unquote(request.headers.get("Directory", ""))
+            if directory is None:
                 return (
-                    jsonify({"success": False, "error": "Internal server error"}),
-                    500,
+                    jsonify({"success": False, "error": "Invalid request data"}),
+                    400,
                 )
+
+            modified_data = modify_func(data, directory, request_data)
+            self.write_data(modified_data)
+            self.read_data.cache_clear()
+            return jsonify({"success": True})
+        except Exception as e:
+            print("Error modifying data:", e)
+            return (
+                jsonify({"success": False, "error": "Internal server error"}),
+                500,
+            )
 
     def set_data(self):
         return self.modify_data(self._set_data)
@@ -104,26 +101,26 @@ class DataHandler:
         return data
 
     def delete_data(self):
-            try:
-                data = self.read_data()
-                directory = unquote(request.headers.get("Directory", ""))
+        try:
+            data = self.read_data()
+            directory = unquote(request.headers.get("Directory", ""))
 
-                if not directory:
-                    return (
-                        jsonify({"success": False, "error": "Invalid request data"}),
-                        400,
-                    )
-
-                modified_data = self._delete_data(data, directory)
-                self.write_data(modified_data)
-                self.read_data.cache_clear()
-                return jsonify({"success": True})
-            except Exception as e:
-                print("Error deleting data:", e)
+            if not directory:
                 return (
-                    jsonify({"success": False, "error": "Internal server error"}),
-                    500,
+                    jsonify({"success": False, "error": "Invalid request data"}),
+                    400,
                 )
+
+            modified_data = self._delete_data(data, directory)
+            self.write_data(modified_data)
+            self.read_data.cache_clear()
+            return jsonify({"success": True})
+        except Exception as e:
+            print("Error deleting data:", e)
+            return (
+                jsonify({"success": False, "error": "Internal server error"}),
+                500,
+            )
 
     @staticmethod
     def _delete_data(data, directory):
